f8a93eece42defaa3e9f23c59ef045ac93bc8e96
Nicolas Bazille <oltodo@msn.com>
2019-05-08 17:20:50 +0200

    chore(deps): bump dependencies

--- app/reducers/types.js
+++ app/reducers/types.js
@@ -1,4 +1,16 @@
-import type { Dispatch as ReduxDispatch, Store as ReduxStore } from 'redux';
+import type {
+  Dispatch as ReduxDispatch,
+  Reducer as ReduxReducer,
+  Store as ReduxStore,
+  CombinedReducer as ReduxCombinedReducer
+} from 'redux';
+
+export type Reducers = {
+  router: ReduxReducer<>,
+  counter: ReduxReducer<number, Action>
+};
+
+export type CombinedReducer = ReduxCombinedReducer<Reducers, Action>;
 
 export type counterStateType = {
   +counter: number
--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -3,8 +3,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
+import type { StoreEnhancer } from 'redux';
 import createRootReducer from '../reducers';
-import type { counterStateType } from '../reducers/types';
+import type { CombinedReducer, counterStateType } from '../reducers/types';
 
 const history = createHashHistory();
 const rootReducer = createRootReducer(history);
@@ -12,7 +13,11 @@ const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
 function configureStore(initialState?: counterStateType) {
-  return createStore(rootReducer, initialState, enhancer);
+  return createStore<CombinedReducer, counterStateType, StoreEnhancer<*, *, *>>(
+    rootReducer,
+    initialState,
+    enhancer
+  );
 }
 
 export default { configureStore, history };
