1f8f304bd86576696b5d19776ebe8386ba8709d5
Nicolas Bazille <oltodo@msn.com>
2019-05-10 13:52:56 +0200

    chore: install and use material-ui

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { CssBaseline, createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 import type { Store } from '../reducers/types';
 import Routes from '../Routes';
 
@@ -10,13 +12,28 @@ type Props = {
   history: {}
 };
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#322E3C'
+    },
+    background: {
+      default: '#322E3C'
+    },
+    type: 'dark'
+  }
+});
+
 export default class Root extends Component<Props> {
   render() {
     const { store, history } = this.props;
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
-          <Routes />
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Routes />
+          </ThemeProvider>
         </ConnectedRouter>
       </Provider>
     );
