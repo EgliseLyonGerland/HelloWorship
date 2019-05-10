d1078796ec61bbe74dc5d23cba10bae6f124164b
Nicolas Bazille <oltodo@msn.com>
2019-05-10 13:48:10 +0200

    chore: remove counter page

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,8 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
-import styles from './Home.css';
 
 type Props = {};
 
@@ -10,11 +7,6 @@ export default class Home extends Component<Props> {
   props: Props;
 
   render() {
-    return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-      </div>
-    );
+    return <div data-tid="container" />;
   }
 }
