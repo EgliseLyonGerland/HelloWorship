301d453116955e64b8e031f6ed886a829cd8cefb
Nicolas Bazille <oltodo@msn.com>
2019-05-10 13:51:03 +0200

    chore: maximize window at startup

--- app/main.dev.js
+++ app/main.dev.js
@@ -69,12 +69,11 @@ app.on('ready', async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
     webPreferences: {
       nodeIntegration: true
     }
   });
+  mainWindow.maximize();
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
 
