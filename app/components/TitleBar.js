24f5cdfb01dcf017579de68241b12b998f3b42ac
Nicolas Bazille <oltodo@msn.com>
2019-06-04 00:38:30 +0200

    feat: ensure titlebar is never resized

--- app/components/TitleBar.js
+++ app/components/TitleBar.js
@@ -4,6 +4,7 @@ import { styled } from '@material-ui/styles';
 const Root = styled('div')(({ theme: { palette } }) => ({
   background: palette.primary.dark,
   height: 24,
+  minHeight: 24,
   borderBottom: [[1, 'black', 'solid']],
   '-webkit-app-region': 'drag',
 }));
