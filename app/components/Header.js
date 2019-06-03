d05dd1d3149bc645f4beac5b9dc22e0ee8c78032
Nicolas Bazille <oltodo@msn.com>
2019-06-04 00:37:19 +0200

    chore: use makeStyles insteadof styled

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { AppBar, Toolbar } from '@material-ui/core';
-import { styled } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/styles';
 import logo from 'images/logo.svg';
 
-const Image = styled('img')({
-  margin: [[0, 'auto']],
-});
+const useStyles = makeStyles({ img: { margin: [[0, 'auto']] } });
 
-export default () => (
-  <AppBar position="relative" color="inherit" elevation={0}>
-    <Toolbar>
-      <Image src={logo} alt="Logo" />
-    </Toolbar>
-  </AppBar>
-);
+export default () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="relative" color="inherit" elevation={0}>
+      <Toolbar>
+        <img className={classes.img} src={logo} alt="Logo" />
+      </Toolbar>
+    </AppBar>
+  );
+};
