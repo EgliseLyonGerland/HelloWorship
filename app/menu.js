066b4882cf9e4fb1c61ca64c95ba87b9819cc9c9
Nicolas Bazille <oltodo@msn.com>
2019-06-15 12:51:15 +0200

    feat: change app name

--- app/menu.js
+++ app/menu.js
@@ -45,17 +45,17 @@ export default class MenuBuilder {
 
   buildDarwinTemplate() {
     const subMenuAbout = {
-      label: 'Electron',
+      label: 'HelloWorship',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About HelloWorship',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide HelloWorship',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
